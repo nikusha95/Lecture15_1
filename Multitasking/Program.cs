6eeceebfc2977cbdcbc6231e48d7caaddf007d76
nikusha95 <nikasanaia11@yahoo.com>
2022-11-20 11:47:57 +0400

    init

---
+++ Multitasking/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Thread t1 = new Thread(M1)
+{
+    Name = "first Thread"
+};
+Thread t2 = new Thread(M2)
+{
+    Name = "second Thread"
+};
+// t1.Start();
+// t1.Join();
+// t2.Start();
+//new Thread(M1).Start();
+Console.WriteLine(2);
+
+
+Thread t = new Thread(Print);
+t.Start("hello");
+
+void Print(object message) => Console.WriteLine(message);
+
+
+var task = Task.Run(() =>
+{
+    Console.WriteLine("Waiting");
+    Thread.Sleep(5000);
+    Console.WriteLine("Hi");
+    int res = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        res += i;
+    }
+    
+    return res;
+});
+//task.Wait();
+var awaiter = task.GetAwaiter();
+
+int x = awaiter.GetResult();
+Console.WriteLine(x);
+
+await Task.Run(() =>
+{
+    Console.WriteLine("Waiting");
+    Thread.Sleep(5000);
+    Console.WriteLine("Hi");
+});
+
+Console.WriteLine(task.Status);
+Console.WriteLine(task.IsCompleted);
+
+
+void M1()
+{
+    object dummy = new object();
+
+    for (int i = 0; i < 10; i++)
+    {
+        lock (dummy)
+        {
+            Console.WriteLine($"Thread id :{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"M1 {i}");
+        }
+    }
+}
+
+void M2()
+{
+    try
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Thread id :{Thread.CurrentThread.ManagedThreadId}");
+            //Console.WriteLine($"Thread name :{Thread.CurrentThread.Name}");
+            Console.WriteLine($"M2 {i}");
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+}
---
+++ StackEcpression/Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+//(1+2)*3*(1+2*3)-4
+
+using System.Reflection.Metadata.Ecma335;
+
+double r = CalculateExpression("(1+2)*3*(1+2*3)"); //9*7 = 63
+
+Console.WriteLine(r);
+
+static double CalculateExpression(string expression)
+{
+    Stack<int> numbers = new Stack<int>();
+    Stack<char> operators = new Stack<char>();
+    Stack<char> brackets = new Stack<char>();
+    double result = 0;
+    foreach (var t in expression)
+    {
+        double tmp = 0;
+        if (IsNumber(t))
+        {
+            if (brackets.Count == 0 && operators.Count != 0)
+            {
+                result = Calculate(result, t - '0', operators.Pop());
+            }
+            else
+            {
+                numbers.Push(t - '0');
+            }
+        }
+        else if (IsOperator(t))
+        {
+            operators.Push(t);
+        }
+        else if (t == '(')
+        {
+            brackets.Push(t);
+        }
+        else if (t == ')')
+        {
+            brackets.Pop();
+            tmp = numbers.Pop();
+            while (numbers.Count != 0)
+            {
+                double num = numbers.Pop();
+                char op = operators.Pop();
+                tmp = Calculate(tmp, num, op);
+            }
+
+            if (operators.Count != 0)
+            {
+                result = Calculate(result, tmp, operators.Pop());
+            }
+            else
+            {
+                result += tmp;
+            }
+        }
+    }
+
+    return result;
+}
+
+static double Calculate(double x, double y, char op)
+{
+    return op switch
+    {
+        '*' => x * y,
+        '/' => x / y,
+        '+' => x + y,
+        '-' => x - y,
+        _ => throw new ArgumentOutOfRangeException(nameof(op), op, null)
+    };
+}
+
+static bool IsOperator(char c)
+{
+    return c is '*' or '/' or '+' or '-';
+}
+
+static bool IsNumber(char c)
+{
+    return c is >= '1' and <= '9';
+}
